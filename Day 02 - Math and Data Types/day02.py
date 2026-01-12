c74e4d654fbd326505052d4be2b3f5a23292dbe9
Jared <jkarpiak@nait.ca>
2026-01-12 11:31:07 -0700

    Added Day 2 and more

---
+++ Day 02 - Math and Data Types/day02.py
@@ -0,0 +1,78 @@
+# integers : whole numbers
+# strings : plain text
+# floats : decimal numbers
+# booleans : True/False
+
+#name = "Bonobo"
+#rating = 4.9
+
+#age = input("How old are you? ")
+#age = int(age) + 1 
+
+#print(age)
+
+# Convert variable to integer: int()
+# Convert variable to float:   float()
+# Convert variable to boolean: bool()
+# Convert variable to string:  str()
+
+# MATH!!!!
+
+# + add
+# - subtract
+# * multiply
+# / division (return a float)
+# ** exponent
+# // integer division
+# % modulus
+
+result = 5 + 3
+print(f"add: {result}")
+
+result = 5 - 3
+print(f"subtract: {result}")
+
+result = 5 * 15
+print(f"multiply: {result}")
+
+result = 5 / 3
+print(f"divide: {result}")
+
+result = 5 ** 3
+print(f"Exponent: {result}")
+
+result = 5 // 3
+print(f"Int division: {result}")
+
+result = 10 % 3
+print(f"Modulus: {result}")
+
+# Order of operations
+# BEDMAS
+# Brackets
+# Exponent
+# Division
+# Multiplication
+# Addition
+# Subtraction
+
+num1 = 5
+num2 = 10
+num3 = 9
+num4 = 3
+
+result = num1 + num2 * num3 - num4
+print(result)
+
+# 5 + 10 * 9 - 3
+# 5 + 90 - 3
+# 95 - 3
+# 92
+
+result = (num1 + num2) * num3 - num4 ** 2
+
+# (5 + 10) * 9 - 3 ** 2
+# 15 * 9 - 3 ** 2
+# 15 * 9 - 9
+# 135 - 9
+# 126
